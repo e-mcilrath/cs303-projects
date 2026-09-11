15d3336398391fb11b8e01c3331711dd7f9bc8b3
Eric McIlrath <edmcilrath@gmail.com>
2026-09-11 13:35:17 -0400

    stl and main

--- proj2/stl.cpp
+++ proj2/stl.cpp
@@ -4,22 +4,24 @@
 
 #include <algorithm>
 #include <iostream>
-// we included 
 #include <vector>
-#include <string>
 
 void stl_sort(List &l, bool numeric) {
-    std::vector<Node*> vN;
-    std::vector<int> vi;
-    Node *temp;
-    for(int i = 0; i < size; i++) {
-        temp = l -> head; 
-        vN.push_back(temp); 
-        vi.push_back(temp->number);
-        head = head->next;
-        
-    }
-    std::sort(vN);
-    std::cout << 
-}
+    std::vector<Node*> nodes;
 
+    for (Node* current = l.head; current != nullptr; current = current->next) {
+        nodes.push_back(current);
+    }
+
+
+    std::sort(nodes.begin(), nodes.end(), \
+    [numeric](Node* a, Node* b) {return a->number < b->number;});
+
+    nodes[nodes.size() - 1]->next = nullptr;
+    l.head = nodes.front();
+
+    for (int i = 0; i < nodes.size() - 1; i++) {
+        nodes[i]->next = nodes[i + 1];
+    }
+
+}
